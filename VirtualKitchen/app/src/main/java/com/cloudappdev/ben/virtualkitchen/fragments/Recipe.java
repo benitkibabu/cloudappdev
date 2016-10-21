cc750817cf556aa1e5cf0ebaf73b622dd86d9fc1
benitkibabu <benitkibabu@gmail.com>
2016-10-21 21:15:53 +0100

    21/10/2016

--- VirtualKitchen/app/src/main/java/com/cloudappdev/ben/virtualkitchen/fragments/Recipe.java
+++ VirtualKitchen/app/src/main/java/com/cloudappdev/ben/virtualkitchen/fragments/Recipe.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +23,8 @@ public class Recipe extends Fragment {
     private String mParam2;
 
    // private OnFragmentInteractionListener mListener;
+    private RecyclerView recyclerView;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     public Recipe() {
     }
