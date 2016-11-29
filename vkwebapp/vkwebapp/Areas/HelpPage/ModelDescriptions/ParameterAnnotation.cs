b8c2db128229b31bfd83a232c1bb04284c5c938c
Benit Kibabu <x08424179@student.ncirl.ie>
2016-11-29 23:57:05 +0000

    update n 29th

---
+++ vkwebapp/vkwebapp/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace vkwebapp.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
